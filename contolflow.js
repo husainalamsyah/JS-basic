df2888d261742cb0f0b4018ce0e34a0a22935720
husainalamsyah <Husain4life@gmail.com>
2019-01-17 10:51:47 +0700

    Javascript basic

---
+++ contolflow.js
@@ -0,0 +1,29 @@
+let jam = 9;
+
+if (jam >= 7 && jam < 14) {
+    console.log('selamat pagi');
+}
+else if (jam >= 14 && jam < 19) {
+    console.log('selamat sore');
+}
+else
+    console.log('selamat malam');
+
+    let peran = 'murid';
+
+switch (peran) {
+    case 'murid':
+    console.log('user murid')
+    break;
+
+case 'guru':
+    console.log('user guru')
+    break;
+
+default:
+    console.log('user asing')
+}
+
+// if (peran === 'murid') console.log('murid');
+// else if (role === 'guru') console.log('guru');
+// else console.log('user asing')
