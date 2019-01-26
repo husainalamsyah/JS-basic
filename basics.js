e0a9807d78f0982ef4f4395079049b949b779473
husainalamsyah <43976170+husainalamsyah@users.noreply.github.com>
2019-01-26 21:21:38 +0700

    Objects

--- basics.js
+++ basics.js
@@ -16,3 +16,16 @@ let umur = 19;
 let persetujuan = true;
 let namaAwal = undefined;
 let pilihWarna = null;
+
+//objects
+let person = {
+    nama: 'husain',
+    umur: 19
+};
+
+person.nama = 'alamsyah';
+
+let seleksi = 'nama';
+person[seleksi] = 'kisanak';
+
+console.log(person.nama);
