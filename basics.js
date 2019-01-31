08bd4f3657b386a1c03bdd6e80ed74be9c416b04
husainalamsyah <43976170+husainalamsyah@users.noreply.github.com>
2019-01-31 11:30:39 +0700

    Arrays

--- basics.js
+++ basics.js
@@ -1,8 +1,8 @@
-// variables
+// vari`ables
 
 let namapertama = "husain";
 let namaakhir = "alamsyah";
-console.log(namapertama, namaakhir);
+// console.log(namapertama, namaakhir);
 
 // constants
 
@@ -29,3 +29,43 @@ let seleksi = 'nama';
 person[seleksi] = 'kisanak';
 
 console.log(person.nama);
+
+// Arrays
+let pilihanWarna = ['crimson', 'navy']
+pilihanWarna[2] = 1;
+console.log(pilihanWarna.length)
+
+
+// CONTROL FLOW
+
+// If..else
+let jam = 9;
+
+if (jam >= 7 && jam < 14) {
+    console.log('selamat pagi');
+}
+else if (jam >= 14 && jam < 19) {
+    console.log('selamat sore');
+}
+else
+    console.log('selamat malam');
+
+    let peran = 'murid';
+
+// Switch..case
+switch (peran) {
+    case 'murid':
+    console.log('user murid')
+    break;
+
+case 'guru':
+    console.log('user guru')
+    break;
+
+default:
+    console.log('user asing')
+}
+
+// if (peran === 'murid') console.log('murid');
+// else if (role === 'guru') console.log('guru');
+// else console.log('user asing')
